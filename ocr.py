7577e8bf120f653218c0105fe36af19eb003995c
SolntsevNS <nikitasergsol@yandex.ru>
2022-10-01 17:23:47 +0300

    Refactored OCR code

--- ocr.py
+++ ocr.py
@@ -1,8 +1,7 @@
-import sys
 from PIL import Image, ImageDraw
 
 
-class Component():
+class Component:
     def __init__(self, x, y):
         self._pixels = [(x, y)]
         self._min_x = x
@@ -16,60 +15,65 @@ class Component():
         self._max_x = max(self._max_x, x)
         self._min_y = min(self._min_y, y)
         self._max_y = max(self._max_y, y)
-    #корзина для распознаваемых образов
+
+    # корзина для распознаваемых образов
     def basket(self):
-        return [(self._min_x, self._min_y), (self._max_x, self._max_y)]
+        return (self._min_x, self._min_y), (self._max_x, self._max_y)
 
 
 def find_components(im):
-    width, height  = im.size
+    width, height = im.size
     components = {}
-    pixel_component = [[0 for y in range(height)] for x in range(width)]
+    pixel_component = [[0 for __ in range(height)] for _ in range(width)]
     equivalences = {}
-    n_components = 0
+    components_number = 0
 
     # первый проход и поиск компонентов
-    for x in range(width):
-        for y in range(height):
+    for i in range(width):
+        for j in range(height):
             # проверка на чёрный пиксел
-            if im.getpixel((x, y)) == (0, 0, 0, 255):
+            if im.getpixel((i, j)) == (0, 0, 0, 255):
                 # поиск пикселов, различающихся по значению
-                component_n = pixel_component[x - 1][y] if x > 0 else 0
-                component_w = pixel_component[x][y - 1] if y > 0 else 0
+                upper_component = pixel_component[i - 1][j] if i > 0 else 0
+                left_component = pixel_component[i][j - 1] if j > 0 else 0
 
-                max_component = max(component_n, component_w)
+                min_component, max_component = sorted((upper_component, left_component))
 
-                if max_component > 0:
-                    new_component = min(filter(lambda i: i > 0, (component_n, component_w)))
-                    if max_component > new_component:
+                if min_component > 0:
+                    new_component = min_component
+
+                    if min_component != max_component:
                         if max_component in equivalences:
                             equivalences[max_component].add(new_component)
                         else:
-                            equivalences[max_component] = set((new_component,))
+                            equivalences[max_component] = {new_component}
+
+                elif max_component > 0:
+                    new_component = max_component
                 else:
-                    n_components += 1
-                    new_component = n_components
+                    components_number += 1
+                    new_component = components_number
 
-                pixel_component[x][y] = new_component
+                pixel_component[i][j] = new_component
 
-    #присваивание всем эквивалентным компонентам одинакового значения
-    for x in range(width):
-        for y in range(height):
-                r = pixel_component[x][y]
-                if r > 0:
-                    while r in equivalences:
-                        r = min(equivalences[r])
+    # присваивание всем эквивалентным компонентам одинакового значения
+    for i in range(width):
+        for j in range(height):
+            r = pixel_component[i][j]
+            if r > 0:
+                while r in equivalences:
+                    r = min(equivalences[r])
 
-                    if not r in components:
-                        components[r] = Component(x, y)
-                    else:
-                        components[r].add(x, y)
+                if r not in components:
+                    components[r] = Component(i, j)
+                else:
+                    components[r].add(i, j)
 
-    return list(components.itervalues())
+    return list(components.values())
 
 
 def main():
-    im = Image.open(r"c:\users\perso\OCR\img\test.png")
+    im = Image.open(r"example.png")
     components = find_components(im)
     draw = ImageDraw.Draw(im)
 
@@ -77,10 +81,11 @@ def main():
         draw.rectangle(r.basket(), outline=(255, 0, 0))
     del draw
 
-    #im.show() #проверка
-    output = file("output.png", "wb")
+    im.show()  # проверка
+    output = open("output.png", "wb")
     im.save(output)
     output.close()
+
 
 if __name__ == "__main__":
     main()
